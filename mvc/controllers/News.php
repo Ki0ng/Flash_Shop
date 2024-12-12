8d77edab42be4dbefe4b0edbe253fcaa80064ae4
toanle <kinle2005@gmail.com>
2024-12-12 14:51:04 +0700

    update

---
+++ mvc/controllers/News.php
@@ -0,0 +1,16 @@
+<?php
+class News {
+    function SayHi() {
+        echo "News - SayHi"; 
+    }
+
+    function SayNo($ho, $ten) {
+        echo $ho. " - " .$ten;
+    }
+
+    function SayBye($trang) { 
+        echo $trang;
+    } 
+}
+
+?>  
