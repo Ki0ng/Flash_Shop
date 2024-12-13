2baa6211a14cbca6d19dba75f8c1978a440ed200
CED-b2005 <duqingping2k5@gmail.com>
2024-12-13 09:02:32 +0700

    done header css

--- mvc/views/compnents/header.php
+++ mvc/views/compnents/header.php
@@ -1,3 +1,8 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="/public/CSS/component/header.css.php">
+<link rel="stylesheet" href="/public/font-awesome-4.7.0/css/font-awesome.min.css">
+
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-lg">
---
+++ public/CSS/components/Header.css.php
@@ -0,0 +1,42 @@
+<style>
+.container-lg {
+}
+
+.container-lg {
+    background: #EFBF5F
+}
+
+.navbar-logo>#logo {
+    width: 7rem;
+    height: 7rem;
+    border: 0.25rem solid white;
+    border-radius: 100%;
+    margin: 1rem;
+}
+
+.navbar-title {
+    text-decoration: none;
+    color: black;
+    margin-right: 1em;
+}
+
+.nav-item {
+    margin: 0.7rem;
+    list-style: none;
+}
+
+.nav-item>a {
+    color: black;
+}
+
+#navbarSupportedContent {
+    font-size: 1.4rem;
+}
+
+#icon-search {
+    position: relative;
+    left: -2.3rem;
+    bottom: -0.48rem;
+}
+
+</style>
