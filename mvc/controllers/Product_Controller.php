e44a44cb7aa1023a7d617a91df621108b8a912b8
toanle <kinle2005@gmail.com>
2024-12-16 01:17:09 +0700

    update product_detail

--- mvc/controllers/Product_Controller.php
+++ mvc/controllers/Product_Controller.php
@@ -1,18 +1,23 @@
 <?php
-    class Product_Controller extends Controller {
-        public function show () {
-            
-        }
+class Product_Controller extends Controller
+{
+    public function show()
+    {
 
-        public function ProductDetail () {
-            $product_model = $this->model("Product");
-
-            $proDetail_data = $product_model->get_proDetail_data();
-            
-            $this->view("Master", [
-                "page" => "ProducDetail",
-                "proDeil_data" => $proDetail_data
-            ]);
-        }
+        $this->view("Master", [
+            "Page" => "Product"
+        ]);
     }
-?>
+
+    public function ProductDetail()
+    {
+        $product_model = $this->model("Product");
+
+        $proDetail_data = $product_model->get_proDetail_data();
+
+        $this->view("Master", [
+            "Page" => "ProductDetail",
+            "proDetail_data" => $proDetail_data
+        ]);
+    }
+}
