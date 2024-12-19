85d7eae48bb7e8f8ad55796ec63af5a4c745aa20
bnuoch thi thuy <bnuochthithuy13032005@gmail.com>
2024-12-19 17:27:04 +0700

    fix_controller

--- mvc/controllers/Product_Controller.php
+++ mvc/controllers/Product_Controller.php
@@ -4,24 +4,48 @@ class Product_Controller extends Controller
     public function show()
     {
 
+
         $product_model = $this->model("Product");
         $product_data = $product_model->get_product_data();
 
-        $this->view("Master", [
+
+        $this->view("master", [
             "Page" => "Product",
             "product_data" => $product_data
         ]);
     }
 
-    public function ProductDetail()
+
+    public function ProductDetail($id)
     {
         $product_model = $this->model("Product");
 
-        $proDetail_data = $product_model->get_proDetail_data();
+
+        $proDetail_data = $product_model->get_proDetail_data($id);
+
 
         $this->view("master", [
             "Page" => "ProductDetail",
             "proDetail_data" => $proDetail_data
         ]);
     }
-}
+    //loc san pham
+    public function getFilteredProducts ($data) {
+        $name = $data['Product_Name'];
+        $price = $data['filter-price'];
+        $categories = $data['filter-categories'];
+        if ($name &&  $price && $categories) {
+             return $this->Product_Model->getFilteredProducts($name, $price, $categories);
+        } elseif ($name && $categories && !$price) {
+             return $this->Product_Model->getProductsByCategories($name, $categories);
+        } elseif ($name && $price) {
+             return $this->Product_Model->getProductsByPrice($name, $price);
+        } elseif ($price && $categories) {
+             return $this->Product_Model->getProductsByPriceCategories ($price, $categories);
+        } elseif ($name) {
+             return $this->Product_Model->getProductsByName ($name);
+        }else {
+             return $this->Product_Model->getProducts();
+        }
+     }
+    }
--- mvc/models/Product_Model.php
+++ mvc/models/Product_Model.php
@@ -3,39 +3,100 @@ class Product_Model extends Database
 {
     public function get_product_data()
     {
-        $conn = new Database(); 
+        $conn = new Database();
+
 
         if ($conn) {
             $database = $conn->getConnection();
 
+
             $sql = "select * from products";
+
 
             $data_raw = $database->query($sql);
 
+
             $data = $data_raw->fetch_all();
+
 
             return $data;
         } else {
             return false;
         }
     }
-
-    public function get_proDetail_data()
+//productDetail
+    public function get_proDetail_data($id)
     {
         $conn = new Database();
+
 
         if ($conn) {
             $database = $conn->getConnection();
 
-            $sql = "select * from products";
+
+            $sql = "select * from products where Product_id = $id";
+
 
             $data_raw = $database->query($sql);
 
-            $data = $data_raw->fetch_all();
+
+            $data = $data_raw->fetch_assoc();
+
 
             return $data;
         } else {
             return false;
         }
+    }
+    // loc san pham
+    protected $table = "products";  
+    public function getFilteredProducts ($name, $price, $categories) {
+        $sql = "SELECT * FROM $this->table
+            WHERE Product_Name LIKE '%$name%'
+            AND (
+                ($price = 0 AND price <= 100) OR
+                ($price = 100 AND price <= 300) OR
+                ($price = 300 AND price <= 500) OR
+                ($price = 500 AND price <= 1000) OR
+                ($price = 1000 AND price > 1000)
+            )
+            AND category = '$categories'
+            ORDER BY price DESC";
+        return $this->queryManyRows($sql);
+    }
+    public function getProductsByCategories ($name, $categories) {
+        $sql = "SELECT * FROM $this->table
+            WHERE Product_Name LIKE '%$name%'
+            AND category = '$categories'";
+        return $this->queryManyRows($sql);
+    }
+    public function getProductsByPrice ($name, $price) {
+        $sql = "SELECT * FROM $this->table
+            WHERE Product_Name LIKE '%$name%'
+            AND (
+                ($price = 0 AND price <= 100) OR
+                ($price = 100 AND price <= 300) OR
+                ($price = 300 AND price <= 500) OR
+                ($price = 500 AND price <= 1000) OR
+                ($price = 1000 AND price > 1000)
+            ) ORDER BY price DESC";
+        return $this->queryManyRows($sql);
+    }
+    public function getProducts () {
+        $sql = "SELECT * FROM $this->table";
+        return $this->queryManyRows($sql);
+    }
+    public function getProductsByPriceCategories ($price, $categories) {
+        $sql = "SELECT * FROM $this->table
+            WHERE  (
+                    ($price = 0 AND price <= 100) OR
+                    ($price = 100 AND price <= 300) OR
+                    ($price = 300 AND price <= 500) OR
+                    ($price = 500 AND price <= 1000) OR
+                    ($price = 1000 AND price > 1000)
+            )
+            AND category = '$categories'
+            ORDER BY price DESC";
+        return $this->queryManyRows($sql);
     }
 }
